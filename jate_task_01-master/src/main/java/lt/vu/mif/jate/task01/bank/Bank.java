0298baf16c670bfb49d7d7e61a92238e057e8f38
avgon <avgoncarov@gmail.com>
2020-03-04 16:59:27 +0200

    Add files via upload

---
+++ jate_task_01-master/src/main/java/lt/vu/mif/jate/task01/bank/Bank.java
@@ -0,0 +1,9 @@
+package lt.vu.mif.jate.task01.bank;
+
+public class Bank {
+
+	public Bank(String string, int i, String string2, String string3, String string4) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
